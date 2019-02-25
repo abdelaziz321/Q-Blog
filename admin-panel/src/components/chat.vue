2026ebb31b1b994dfdac5e277e6c0d1852883df6
abdelaziz321 <abdelazizmahmoud321@gmail.com>
2019-02-25 08:56:28 +0200

    using vuex to handle retrieving the [new, previous] messages

--- admin-panel/src/components/chat.vue
+++ admin-panel/src/components/chat.vue
@@ -9,7 +9,12 @@
         <li class="d-flex">
           <div class="messages_status">
             <span v-if="isChatEnded">no more messages</span>
-            <button v-else @click="loadMessages" type="button" >load more messages</button>
+            <button 
+              v-else
+              :class="{'d-none': isMessagesLoading}"
+              @click="loadMessages"
+              type="button"
+            >load more messages</button>
           </div>
         </li>
 
@@ -45,8 +50,6 @@
 </template>
 
 <script>
-import { db } from '@/config/db.js';
-
 export default {
   name: 'chat',
 
@@ -59,84 +62,36 @@ export default {
 
       isChatEnded: false,
       isChatClosed: false,
-
-      lastMessage: null,
-      messages: [],
+      isMessagesLoading: true
     };
   },
 
 
+  computed: {
+    messages: function () {
+      return this.$store.getters['chat/messages'];
+    }
+  },
+
+
   created() {
-    db.collection('messages')
-      .orderBy("created", "desc")
-      .limit(5)
-      .get()
-    .then((messages) => {
-      this.addMessages(messages)
-      .then(() => {
-        this.$refs.messages.scrollTop = this.$refs.messages.scrollHeight;
-      });
+    this.$store.dispatch('chat/getMessages')
+    .then((noMoreMessages) => {
+      this.isChatEnded = noMoreMessages;
+      this.$refs.messages.scrollTop = this.$refs.messages.scrollHeight;
+      this.$store.dispatch('chat/getNewMessages');
+      this.isMessagesLoading = false;
     });
   },
 
 
   methods: {
-    /**
-     * load more messages when the user scroll up
-     */
     loadMessages() {
-      db.collection('messages')
-        .orderBy("created", "desc")
-        .startAfter(this.lastMessage)
-        .limit(5)
-        .get()
-      .then((messages) => {
-        this.addMessages(messages);
-      });
-    },
-
-    /**
-     * add the retrieved messages to the chat
-     */
-    addMessages(messages) {
-      const messagesIds = [];
-      const usersIds = new Set();
-
-      messages.forEach(message => {
-        this.messages.unshift({
-          id: message.id,
-          body: message.data().body,
-          user_id: message.data().user_id
-        });
-
-        messagesIds.push(message.id);
-        usersIds.add(message.data().user_id);
-      });
-
-      if (messages.docs.length < 5) {
-        this.isChatEnded = true;
-      }
-
-      this.lastMessage = messages.docs[messages.docs.length - 1];
-      return this.setChatUsers(messagesIds, usersIds);  
-    },
-
-    /**
-     * get the chat users from the API if not exist in our $store
-     * then add them to the chat messages
-     * 
-     * @param array messagesIds
-     * @param Set   usersIds
-     */
-    setChatUsers(messagesIds, usersIds) {
-      return this.$store.dispatch('chat/getUsers', usersIds)
-      .then(() => {
-        let users = this.$store.getters['chat/users'];
-        
-        this.messages.forEach((message, index) => {
-          message.user = users.get(message.user_id);
-          Vue.set(this.messages, index, message);
-        });
+      this.isMessagesLoading = true;
+      this.$store.dispatch('chat/getMessages')
+      .then((noMoreMessages) => {
+        this.isChatEnded = noMoreMessages;
+        this.isMessagesLoading = false;
       });
     }
   }
--- admin-panel/src/store/modules/chat.js
+++ admin-panel/src/store/modules/chat.js
@@ -1,33 +1,123 @@
 import axios from 'axios';
-import { reject } from 'q';
+import { db } from '@/config/db.js';
+import firebase from 'firebase/app';
+
+const LIMIT = 5;
 
 // initial state
 const state = {
+  messages: [],
+  firstMessage: null,  // refers to the message at the top of our chat
+  lastMessage: null,   // refers to the message at the bottom of our chat
+  
   users: new Map()
 }
 
 // getters
 const getters = {
-  users: (state) => {
-    return state.users;
+  messages: (state) => {
+    return state.messages;
   }
 }
 
 // mutations
 const mutations = {
-  SET_USERS(state, users) {
+  ADD_USERS(state, users) {
     users.forEach((user) => {
       state.users.set(user.id, {
         name: user.username,
         avatar: user.avatar
       });
     });    
+  },
+  
+  ADD_MESSAGES(state, messages) {
+    messages.forEach((message) => {   // (doc)
+      state.messages.unshift({
+        id: message.id,
+        body: message.data().body,
+        user_id: message.data().user_id
+      });
+    });
+  },
+
+  ADD_MESSAGE(state, message) {
+    state.messages.push({
+      id: message.id,
+      body: message.data().body,
+      user_id: message.data().user_id
+    })
+  },
+
+  // TODO: only add the user for the retrieved messages
+  UPDATE_MESSAGES_USERS(state, messages) {
+    state.messages.forEach((message, index) => {
+      if (typeof message.user !== 'undefined') {
+        return;
+      }
+
+      message.user = state.users.get(message.user_id);
+      Vue.set(state.messages, index, message);
+    });
+  },
+
+  UPDATE_FIRST_MESSAGE(state, messages) {
+    state.firstMessage = messages.docs[messages.docs.length - 1];
+  },
+  
+  UPDATE_LAST_MESSAGE(state, messages) {
+    state.lastMessage = messages.docs[0];
   }
 }
 
 // actions
 const actions = {
-  getUsers({ state, commit }, usersIds) {
+  getMessages({ state, commit, dispatch }) {
+    let query = db.collection('messages')
+      .orderBy("created", "desc");
+
+    if (state.firstMessage !== null) {
+      query = query.startAfter(state.firstMessage);
+    }
+
+    return query.limit(LIMIT).get()
+      .then((messages) => {     // querySnapshot
+        commit('ADD_MESSAGES', messages);
+
+        commit('UPDATE_LAST_MESSAGE', messages);
+        commit('UPDATE_FIRST_MESSAGE', messages);
+
+        dispatch('getUsers', messages);
+        return messages.docs.length < LIMIT;
+      });
+  },
+
+  // TODO: react to every new message and notify somehow to our user
+  getNewMessages({ commit, dispatch }) {
+    db.collection("messages")
+      .orderBy("created", "asc")
+      .startAfter(state.lastMessage)
+      .onSnapshot(function (snapshot) {
+        snapshot.docChanges().forEach(function (change) {
+          if (change.type === "added") {
+            let message = change.doc;
+            if (message.exists) {
+              commit('ADD_MESSAGE', message);
+              dispatch('getUsers', [message]);
+            }
+          }
+        });
+      });
+  },
+
+  getUsers({ state, commit }, messages) {
+    // get the users ids from the retrieved messages
+    let usersIds = new Set();
+    messages.forEach((message) => {
+      usersIds.add(message.data().user_id);
+    });
+
+    // get the users ids that we dont have them in the current state
     let missidUsers = [];
     usersIds.forEach((id) => {
       if (!state.users.has(id)) {
@@ -35,10 +125,14 @@ const actions = {
       }
     });
 
+    // there is no need to get users from API,
+    // just update messages users from our Map
     if (missidUsers.length === 0) {
+      commit('UPDATE_MESSAGES_USERS', messages);
       return;
     }
 
+    // build the query to get the missed users from our API
     let query = '';
     missidUsers.forEach((id) => {
       query += `ids[]=${id}&`;
@@ -48,9 +142,9 @@ const actions = {
     return axios.get('/admin/chat/users?' + query)
     .then((response) => {
       let res = response;
-      commit('SET_USERS', res.data);
-    })
-    .catch(() => {});
+      commit('ADD_USERS', res.data);
+      commit('UPDATE_MESSAGES_USERS', messages);
+    });
   }
 }
 
