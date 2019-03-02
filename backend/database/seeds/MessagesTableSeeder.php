7e30274dc97bc8c9f78ff62d38918d63944936e7
abdelaziz321 <abdelazizmahmoud321@gmail.com>
2019-03-02 14:49:20 +0200

    add messages migration and seeder

---
+++ backend/database/seeds/MessagesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\User;
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ChatSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        foreach (range(1, 250) as $id) {
+	        DB::table('messages')->insert([
+	            'message'    => $faker->paragraph(1),
+                'user_id'    => $faker->numberBetween(1, 6),
+                'created_at' => "$faker->date $faker->time"
+	        ]);
+        }
+    }
+}
